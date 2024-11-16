0262e0aec9de6b73cf86fc71d6090a5a3852fd98
viachtravel007 <viachtravel007@gmail.com>
2024-11-16 20:21:37 +0200

    Add method Increment

--- src/viachtravel007/lesson/lesson_4.java
+++ src/viachtravel007/lesson/lesson_4.java
@@ -2,7 +2,38 @@ package viachtravel007.lesson;
 
 public class lesson_4 {
     public static void main(String[] args) {
-        BasicMathExample();
+//        BasicMathExample();
+//        MobExample();
+        Increment();
+    }
+
+    private static void Increment() {
+        int a = 123;
+        a += 100;
+        a += + 100 / 3 - 1;
+        System.out.println("a = " + a);
+
+        a++;
+        System.out.println("a = " + a);
+
+        ++a;
+        System.out.println("a = " + a);
+
+        a = 10;
+        int result = 3 + a++; // 3 + ++a = 14
+        System.out.println("result = " + result);
+
+        result = 10;
+        result = result++ + --result;
+        System.out.println("result = " + result);
+    }
+
+    private static void MobExample() {
+        int a = 15;
+        int b = 8;
+
+        int result = a % b;
+        System.out.println("15 mob 8 == " + result);
     }
 
     public static void BasicMathExample() {
