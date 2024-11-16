a808fe4e94ca8e200cb1a53ced1e7b831062b895
viachtravel007 <viachtravel007@gmail.com>
2024-11-16 19:49:59 +0200

    Add elementary arithmetic operators

--- src/viachtravel007/lesson/lesson_4.java
+++ src/viachtravel007/lesson/lesson_4.java
@@ -1,4 +1,17 @@
 package viachtravel007.lesson;
 
 public class lesson_4 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+
+        double result = (double) a / b;
+        System.out.println("division = " + result);
+
+        result = a + b;
+        System.out.println("addition = " + result);
+
+        result = a - b;
+        System.out.println("subtraction = " + result);
+    }
 }
