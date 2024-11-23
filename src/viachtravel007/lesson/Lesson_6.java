c1a11ac2f3673521c007cfdea609d4dff1e2188f
viachtravel007 <viachtravel007@gmail.com>
2024-11-23 20:41:29 +0200

    Lesson_6 - add Multiplication table

--- src/viachtravel007/lesson/Lesson_6.java
+++ src/viachtravel007/lesson/Lesson_6.java
@@ -4,6 +4,21 @@ import java.util.Scanner;
 
 public class Lesson_6 {
     public static void main(String[] args) {
+//        Non_integer_and_integer();
+        Multiplication_table();
+    }
+
+    private static void Multiplication_table() {
+        for (int i = 1; i < 10; i++) {
+            for (int j = 1; j < 10; j++) {
+                int value = i * j;
+                System.out.printf("%5d", value);
+            }
+            System.out.println();
+        }
+    }
+
+    private static void Non_integer_and_integer() {
         Scanner scanner = new Scanner(System.in);
         int bound = scanner.nextInt();
 
@@ -12,7 +27,7 @@ public class Lesson_6 {
         }
 
         for (int i = 1; i <= bound; i++) {
-            System.out.println(i + i - 2);
+            System.out.println(i + i - 1);
         }
     }
 }
