247b5ccc7f382294e07c01fa3fd4f4ba89e9573c
viachtravel007 <viachtravel007@gmail.com>
2024-11-23 21:52:26 +0200

    OnlineShop - Grammatical and calculation errors have been corrected.

--- src/viachtravel007/homework/OnlineShop.java
+++ src/viachtravel007/homework/OnlineShop.java
@@ -3,10 +3,10 @@ package viachtravel007.homework;
 public class OnlineShop {
     public static void main(String[] args) {
         System.out.println("Shop-online for customers");
-        System.out.println("Tobay only, we have 20% off on all product.");
+        System.out.println("Today only, we have 20% off on all products.");
         int price = 400;
         float discount = 0.2F;
-        float discounted_price = price + (discount * price);
+        float discounted_price = price - (discount * price);
         System.out.println("Only today, get the Acer Aspire 5 for just $" + discounted_price + "!!!");
     }
 }
