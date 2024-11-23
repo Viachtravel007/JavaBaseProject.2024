c3917a88e667b2a0cdd9a7021dcc90c8067947a7
viachtravel007 <viachtravel007@gmail.com>
2024-11-23 21:51:25 +0200

    OnlineShop - The online store ad output has been added.

--- src/viachtravel007/homework/OnlineShop.java
+++ src/viachtravel007/homework/OnlineShop.java
@@ -2,6 +2,11 @@ package viachtravel007.homework;
 
 public class OnlineShop {
     public static void main(String[] args) {
-        System.out.println("This is shop-online app");
+        System.out.println("Shop-online for customers");
+        System.out.println("Tobay only, we have 20% off on all product.");
+        int price = 400;
+        float discount = 0.2F;
+        float discounted_price = price + (discount * price);
+        System.out.println("Only today, get the Acer Aspire 5 for just $" + discounted_price + "!!!");
     }
 }
