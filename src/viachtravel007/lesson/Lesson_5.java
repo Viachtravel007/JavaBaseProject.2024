1f77e65066589ee3a1b452909c023c77eb79afc9
viachtravel007 <viachtravel007@gmail.com>
2024-11-23 13:19:04 +0200

    Lesson_5 - add method ConditionDemo.

--- src/viachtravel007/lesson/Lesson_5.java
+++ src/viachtravel007/lesson/Lesson_5.java
@@ -3,8 +3,24 @@ package viachtravel007.lesson;
 public class Lesson_5 {
     public static void main(String[] args) {
 //        RationalOperators();
-        TernaryOperator();
+//        TernaryOperator();
+        ConditionDemo();
     }
+
+    private static void ConditionDemo() {
+        int a = 10;
+        int b = 1;
+
+        if (a % 2 == 0) {
+            if (b % 2 == 0) {
+                System.out.println("00");
+            } else {
+                System.out.println("Unexpected");
+            }
+
+        }
+    }
+
     public static void RationalOperators() {
         int a = 10;
         int b = -123;
@@ -26,14 +42,10 @@ public class Lesson_5 {
     }
     public static void TernaryOperator() {
         int result;
-        int a = 10;
+        int a = 40;
         int b = 25;
 
-        if (a > b) {
-            result = 1;
-        } else {
-            result = -1;
-        }
+        result = (a > b) ? a % 2 == 0 ? 123: -123 : -1;
 
         System.out.println(result);
     }
