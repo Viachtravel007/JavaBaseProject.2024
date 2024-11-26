4555ecbfa026247a7a8a615c145e567c221c6d7a
viachtravel007 <viachtravel007@gmail.com>
2024-11-26 21:47:26 +0200

    CalculatingSumNumbers - improved loop program

--- src/viachtravel007/homework/CalculatingSumNumbers.java
+++ src/viachtravel007/homework/CalculatingSumNumbers.java
@@ -3,8 +3,28 @@ package viachtravel007.homework;
 import java.util.Scanner;
 
 public class CalculatingSumNumbers {
+
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Write which loop you want to use (For, While, Do-While)");
+        String method = scanner.nextLine();
+
+        if (method.equalsIgnoreCase("For")) {
+            ForLoops();
+        } else if (method.equalsIgnoreCase("While")) {
+            WhileLoops();
+        } else if (method.equalsIgnoreCase("Do-While")) {
+            DoWhileLoops();
+        } else {
+            System.out.println("Invalid input. Please write For, While, or Do-While.");
+        }
+
+    }
+
+    public static void ForLoops() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write any number");
         int number = scanner.nextInt();
         int sum = 0;
         for (int i = 1; i <= number; i++) {
@@ -14,4 +34,35 @@ public class CalculatingSumNumbers {
         System.out.println("-----------------------------");
         System.out.println("Sum of numbers is " + sum);
     }
+
+    public static void WhileLoops() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write any number");
+        int number = scanner.nextInt();
+        int sum = 0;
+        int i = 1;
+        while (i <= number) {
+            sum += i;
+            System.out.println(i + ") Num is " + i + ", sum is " + sum);
+            i++;
+        }
+        System.out.println("-----------------------------");
+        System.out.println("Sum of numbers is " + sum);
+    }
+
+    public static void DoWhileLoops() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write any number");
+        int number = scanner.nextInt();
+        int sum = 0;
+        int i = 1;
+        do  {
+            sum += i;
+            System.out.println(i + ") Num is " + i + ", sum is " + sum);
+            i++;
+        } while (i <= number);
+        System.out.println("-----------------------------");
+        System.out.println("Sum of numbers is " + sum);
+    }
+
 }
