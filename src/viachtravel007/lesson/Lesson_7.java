98457c300fbdad99380ac9a97fe1f0434a416a23
viachtravel007 <viachtravel007@gmail.com>
2024-11-28 20:36:27 +0200

    Create class Lesson_7. Add common arrays

---
+++ src/viachtravel007/lesson/Lesson_7.java
@@ -0,0 +1,13 @@
+package viachtravel007.lesson;
+
+public class Lesson_7 {
+    public static void main(String[] args) {
+
+        int[] price = {
+                12, 156, 90, 1
+        };
+        for (int i = 0; i < price.length; i++) {
+            System.out.println("price#" + i + " == " + price[i]);
+        }
+    }
+}
