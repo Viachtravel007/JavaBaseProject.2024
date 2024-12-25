5e74ac66eb504d718d613b2bf3b8861dcc5d58d6
viachtravel007 <viachtravel007@gmail.com>
2024-12-25 22:15:38 +0200

    create Person and PersonDescribe class with constructor and methods for managing person details

---
+++ src/viachtravel007/homework/personal_data/Person.java
@@ -0,0 +1,29 @@
+package viachtravel007.homework.personal_data;
+
+public class Person {
+    private String name;
+    private int age;
+    private String profession;
+
+    public Person(String name, int age, String profession) {
+        this.name = name;
+        this.age = age;
+        this.profession = profession;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String newProfession) {
+        this.profession = newProfession;
+    }
+}
